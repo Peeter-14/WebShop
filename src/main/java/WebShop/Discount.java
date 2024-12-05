f7271c65d7bd7562a88413962b6fb0f9dd01f30d
Peeter-14 <vati.peeter@gmail.com>
2024-12-05 11:12:20 +0100

    Update Discount.java

--- src/main/java/WebShop/Discount.java
+++ src/main/java/WebShop/Discount.java
@@ -1,4 +1,16 @@
 package WebShop;
 
 public class Discount {
+   /**
+     * @param price
+     * @param discount
+     * @return
+     * @throws ArithmeticException
+     */
+    public double dis(double price, double discount) throws ArithmeticException {
+        if (discount == 0) {
+            throw new ArithmeticException("The discount can not be null!");
+        }
+        return (double) price * (discount / 100);
+    }
 }
